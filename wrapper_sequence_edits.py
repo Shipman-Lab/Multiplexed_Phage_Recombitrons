694f44c2b7ea1639a85085ed19de096c9c2817cf
Chloe Fishman <cfishman@LAXUDB08089.local>
2022-05-13 15:14:48 -0700

    first draft of wrapper, directed output to csv

--- wrapper_sequence_edits.py
+++ wrapper_sequence_edits.py
@@ -8,18 +8,32 @@ import pandas as pd
               #"fastq_name2": [L_fuzzy_search, R_fuzzy_search, L_to_R_length, edit_position]}
 
 # (for testing)
-#fastq_dict = {"msSBK-27-29_S29_L001_R1_001.fastq": 0}
+fastq_dict = {"msSBK-27-29_S29_L001_R1_001.fastq": [TGTAAGTTCCGCAATAACGT, CGATGTGCGCCAGCGGAGTC, 90, 45],
+              "msSBK-27-29_S29_L001_R2_001.fastq": [TGTAAGTTCCGCAATAACGT, CGATGTGCGCCAGCGGAGTC, 90, 45]}
              
 
 # direct it to where the reads are
 data_loc = "./data/"  
 
 # loop through all the fastqs
-# handle is the fastq 
+# handle is the opened fastq file
+# each read is a record in iterator
+# iterator (big list that you can loop through) is made by SeqIO.parse 
 for fastq_name in fastq_dict.keys():
     print(fastq_name)
     with open(data_loc+fastq_name) as handle:
-        for record in SeqIO.parse(handle, "fastq"):
+        df = pd.DataFrame(index = SeqIO.index(handle, 'fastq'), columns = ["does_it_map", "correct_length", "nt_at_edit_pos"])
+        for read in SeqIO.parse(handle, "fastq"):
+            L_fuzzy_search = fastq_dict[fastq_name][0]
+            R_fuzzy_search = fastq_dict[fastq_name][1]
+            L_to_R_length = fastq_dict[fastq_name][2]
+            edit_position = fastq_dict[fastq_name][3]
+            output = determine_sequence_edits(read, L_fuzzy_search, R_fuzzy_search, L_to_R_length, edit_position)
+            df.loc[read.id, "does_it_map"] = output['does_it_map']
+            df.loc[read.id, "correct_length"] = output['correct_length']
+            df.loc[read.id, "nt_at_edit_pos"] = output['nt_at_edit_pos']
+        df.to_csv(data_loc + fastq_name[:-6] + "_processed_data.csv")
+
             #import pdb 
             #pdb.set_trace()
 
