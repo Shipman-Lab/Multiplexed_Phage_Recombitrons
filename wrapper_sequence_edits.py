838f229482e629834d71a8e915798869380d523e
Chloe Fishman <cfishman@LAXUDB08089.local>
2022-05-13 16:00:51 -0700

    de-bugged first version of wrapper

--- wrapper_sequence_edits.py
+++ wrapper_sequence_edits.py
@@ -8,8 +8,7 @@ import pandas as pd
               #"fastq_name2": [L_fuzzy_search, R_fuzzy_search, L_to_R_length, edit_position]}
 
 # (for testing)
-fastq_dict = {"msSBK-27-29_S29_L001_R1_001.fastq": [TGTAAGTTCCGCAATAACGT, CGATGTGCGCCAGCGGAGTC, 90, 45],
-              "msSBK-27-29_S29_L001_R2_001.fastq": [TGTAAGTTCCGCAATAACGT, CGATGTGCGCCAGCGGAGTC, 90, 45]}
+fastq_dict = {"msSBK-29-37_S37_L001_R1_001.fastq": ["TGTAAGTTCCGCAATAACGT", "CGATGTGCGCCAGCGGAGTC", 90, 45]}
              
 
 # direct it to where the reads are
@@ -19,16 +18,17 @@ data_loc = "./data/"
 # handle is the opened fastq file
 # each read is a record in iterator
 # iterator (big list that you can loop through) is made by SeqIO.parse 
+
 for fastq_name in fastq_dict.keys():
     print(fastq_name)
     with open(data_loc+fastq_name) as handle:
-        df = pd.DataFrame(index = SeqIO.index(handle, 'fastq'), columns = ["does_it_map", "correct_length", "nt_at_edit_pos"])
+        df = pd.DataFrame(index = SeqIO.index(data_loc + fastq_name, 'fastq'), columns = ["does_it_map", "correct_length", "nt_at_edit_pos"])
         for read in SeqIO.parse(handle, "fastq"):
             L_fuzzy_search = fastq_dict[fastq_name][0]
             R_fuzzy_search = fastq_dict[fastq_name][1]
             L_to_R_length = fastq_dict[fastq_name][2]
             edit_position = fastq_dict[fastq_name][3]
-            output = determine_sequence_edits(read, L_fuzzy_search, R_fuzzy_search, L_to_R_length, edit_position)
+            output = determine_sequence_edits(str(read.seq), L_fuzzy_search, R_fuzzy_search, L_to_R_length, edit_position)
             df.loc[read.id, "does_it_map"] = output['does_it_map']
             df.loc[read.id, "correct_length"] = output['correct_length']
             df.loc[read.id, "nt_at_edit_pos"] = output['nt_at_edit_pos']
