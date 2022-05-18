4a224c5fb65c439cb6dc3031a1ee5e02e7f20d40
Chloe Fishman <cfishman@LAXUDB08089.local>
2022-05-18 14:48:11 -0700

    updated to include multiple edits

--- determine_sequence_edits.py
+++ determine_sequence_edits.py
@@ -1,17 +1,19 @@
 from Bio import SeqIO
 import fuzzysearch
 
-def determine_sequence_edits(read, L_fuzzy_search, R_fuzzy_search, L_to_R_length, edit_position, fuzziness=4):
+def determine_sequence_edits(read, L_fuzzy_search, R_fuzzy_search, L_to_R_length, edit_position_array, fuzziness=1):
     """
     This function is determining whether the reads map to a 
-    region of interest and if there is an edit at a certain base position within that region. 
+    region of interest and if there is an edit at a certain base position within that region.
+    It can be extended to look for multipe edits, but it doesn't do that currently. 
 
     Inputs: Read is a single read, L_fuzzy_search is the left side of read that contains the WT genome, 
     R_fuzzy_search is the right side of read that contains the WT genome, 
     fuzziness is how many differences from the WT sequence are allowed,
     L_to_R_length is how long the region of interest is, 
     edit_position_array is the base position of where to look for edits, 
-
+            
+            example -- 
             for L-tail edits with pSBK.148:
             L_fuzzy_search: TGTAAGTTCCGCAATAACGT
             R_fuzzy_search: CGATGTGCGCCAGCGGAGTC
@@ -23,7 +25,7 @@ def determine_sequence_edits(read, L_fuzzy_search, R_fuzzy_search, L_to_R_length
              correct_length is true if the L_to_R_length fits = the search length 
              nt_at_edit_pos is the nucleotide at the edit position
 
-             use wrapper script to assess all reads and count edits 
+             use wrapper script to cycle through all reads and count edits 
 
     """
 
@@ -44,11 +46,14 @@ def determine_sequence_edits(read, L_fuzzy_search, R_fuzzy_search, L_to_R_length
     else:
         correct_length = (L_to_R_length == R_fuzzy_match[0].start - L_fuzzy_match[0].end) 
         if correct_length == True: 
-            string = read[L_fuzzy_match[0].end:R_fuzzy_match[0].start]
-            nt_at_edit_pos = string[edit_position-1]
+            chopped_read = read[L_fuzzy_match[0].end:R_fuzzy_match[0].start]
+            nt_array = []
+            for base_position in edit_position_array: 
+                nt_at_edit_pos = chopped_read[base_position-1]
+                nt_array.append(nt_at_edit_pos)
             return {"does_it_map": does_it_map,
                     "correct_length": correct_length,
-                    "nt_at_edit_pos": nt_at_edit_pos}
+                    "nt_at_edit_pos": nt_array}
         else:
             nt_at_edit_pos = "N"
             correct_length = False
