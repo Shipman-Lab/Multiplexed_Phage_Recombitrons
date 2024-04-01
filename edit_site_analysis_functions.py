acca41522308a883994745438f3e7428b4dde638
Kate Crawford <kate.crawford@gladstone.ucsf.edu>
2024-04-01 11:37:18 -0700

    changed the mismatch rate to allow for PAM change

--- edit_site_analysis_functions.py
+++ edit_site_analysis_functions.py
@@ -3,11 +3,12 @@ import os
 from pathlib import Path
 
 def extract_and_match(read, L_outside, R_outside, L_inside, R_inside, wt_nt, edited_nt):
-    left_outside = fuzzysearch.find_near_matches(L_outside, read, max_l_dist=2)
-    right_outside = fuzzysearch.find_near_matches(R_outside, read, max_l_dist=2)
+    left_outside = fuzzysearch.find_near_matches(L_outside, read, max_l_dist=4)
+    right_outside = fuzzysearch.find_near_matches(R_outside, read, max_l_dist=4)
     if len(left_outside) == 1 and len(right_outside) == 1:
-        left_inside = fuzzysearch.find_near_matches(L_inside, read, max_l_dist=1)
-        right_inside = fuzzysearch.find_near_matches(R_inside, read, max_l_dist=1)
+        # allow two mismatches for the PAM edit
+        left_inside = fuzzysearch.find_near_matches(L_inside, read, max_l_dist=2)
+        right_inside = fuzzysearch.find_near_matches(R_inside, read, max_l_dist=2)
         if len(left_inside) == 1 and len(right_inside) == 1:
             var_nt = read[left_inside[0].end:right_inside[0].start]
             if var_nt == wt_nt:
