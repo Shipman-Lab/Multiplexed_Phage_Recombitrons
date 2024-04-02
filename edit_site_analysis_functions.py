8d939da2e95ef168aed82610917cb0f99d0a76e8
Kate Crawford <kate.crawford@gladstone.ucsf.edu>
2024-04-02 14:43:27 -0700

    fixed bug where sometimes the inside matched the bad quality read at the end of the read

--- edit_site_analysis_functions.py
+++ edit_site_analysis_functions.py
@@ -9,14 +9,19 @@ def extract_and_match(read, L_outside, R_outside, L_inside, R_inside, wt_nt, edi
         # allow two mismatches for the PAM edit
         left_inside = fuzzysearch.find_near_matches(L_inside, read, max_l_dist=2)
         right_inside = fuzzysearch.find_near_matches(R_inside, read, max_l_dist=2)
-        if len(left_inside) == 1 and len(right_inside) == 1:
-            var_nt = read[left_inside[0].end:right_inside[0].start]
-            if var_nt == wt_nt:
-                return 'wt'
-            elif var_nt == edited_nt:
-                return 'edited'
-            else:
-                return 'unmatched_edit_nt'
+        # check all combinations of matches, because sometimes the inside search seq
+        # happens in the bad end of the read and then there are more than two matches >:(
+        if (len(left_inside) >= 1) & (len(right_inside) >= 1):
+            for left_match in left_inside:
+                for right_match in right_inside:
+                    var_nt = read[left_match.end:right_match.start]
+                    if len(var_nt) == 1:
+                        if var_nt == wt_nt:
+                            return 'wt'
+                        elif var_nt == edited_nt:
+                            return 'edited'
+                        else:
+                            return 'unmatched_edit_nt'
         else:
             return 'unmatched_region'
     else:
