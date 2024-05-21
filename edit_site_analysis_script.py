65d51f2bce861a8848a2e21759967493d651dc29
Asim Khan <asim.khan@gladstone.ucsf.edu>
2024-05-21 13:46:55 -0700

    updated script with file info

--- edit_site_analysis_script.py
+++ edit_site_analysis_script.py
@@ -28,7 +28,7 @@ from edit_site_analysis_functions import extract_and_match, find_file
 # run_path = os.path.expanduser("~/Volumes/Shipman-Lab/BaseSpace/msDMP_01-705234547")
 # if the run path is on the hive
 run_path = r"C:\BaseSpace"
-run_name = "msMRM_12"
+run_name = "msAGK_25"
 # this check is to get rid of the silent error where the script can't
 # find your files because the Hive isn't mounted
 # needs to be changed to the correct path if you're not on a Mac
@@ -45,7 +45,7 @@ git_short_hash = str(git_short_hash, "utf-8").strip()
 
 # load in file key
 # if you don't care about some of these, just leave them blank in the file key
-file_key = pd.read_excel("phage_MRM_12_filekey.xlsx")\
+file_key = pd.read_excel("phage_AGK_25_filekey.xlsx")\
            [["run_id", "info", "plasmid", "wt_nt", "edit_nt",
              "L_inside", "R_inside", "L_outside", "R_outside"]]
 outcome_df = file_key.copy()
